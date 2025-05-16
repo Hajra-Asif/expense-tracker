4ee3731fb6e418b839ecb13ff91b174299990634
Hajra-Asif <hajramasif@gmail.com>
2025-05-16 21:09:25 +0500

    somechanges

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-// src/config.js
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -8,4 +10,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-export default firebaseConfig;
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,10 @@
-import firebaseConfig from "./config"
+import app from './config'
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
