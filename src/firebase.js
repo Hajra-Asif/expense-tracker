237c074186c94b2ad4b1984f3d92e104b0056838
Hajra-Asif <hajramasif@gmail.com>
2025-05-16 21:03:50 +0500

    change

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import firebaseConfig from "./config";
+import firebaseConfig from "./config"
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
